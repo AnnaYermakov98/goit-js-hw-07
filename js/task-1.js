7d4a71b6f7c098fd17680ccb712c38c3d99d3a1c
AnnaYermakov98 <annaermakovic3@gmail.com>
2024-07-27 14:30:48 +0200

    fix hw

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,10 @@
 const getItem = document.querySelectorAll(".item");
-console.log(getItem.length)
+console.log(`Categories: ${getItem.length}`)
+
+getItem.forEach(item => {
+    const header = item.querySelector("h2");
+    const getList = item.querySelectorAll("li");
+    console.log(`Category: ${header.textContent}`);
+    console.log(`Elements: ${getList.length}`);
+    
+});
--- js/task-6.js
+++ js/task-6.js
@@ -4,32 +4,42 @@ const createBtn= document.querySelector("[data-create]");
 const destroyBtn = document.querySelector("[data-destroy]");
 const boxesContainer = document.querySelector("#boxes");
 
-destroyBtn.addEventListener("click", destroyBoxes);
-createBtn.addEventListener("ckick", createBoxes);
+destroyBtn.addEventListener("click", destroyHandler);
+createBtn.addEventListener("click", createHandler);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-function createBoxes (amount) {
-  boxesContainer.innerHTML = "";
+
+function createBoxes(amount) {
+  destroyHandler();
+  amount = Number(amount);
+  let size = 30;
+  const boxesArray = [];
   
   for (let i = 0; i < amount; i++) {
-    const div = document.createElement("div");
-    const size = 30 + i * 10;
-    div.style.width = `${size}px`;
-    div.style.height = `${size}px`;
-    div.style.backgroundColor = getRandomHexColor();
-    boxesContainer.appendChild(div);
+    boxesArray.push(`<div 
+     style="width: ${size}px;
+     height: ${size}px;
+     background-color: ${getRandomHexColor()};">
+     </div>`);
+    size += 10;
   }
-}
-function destroyBoxes() {
+    boxesContainer.insertAdjacentHTML("beforeend", boxesArray.join(""));
+  }
+    
+function destroyHandler() {
   boxesContainer.innerHTML = "";
 }
-createBtn.addEventListener("click", () => {
-  const amount = parseInt(input.value);
+
+function createHandler() {
+  const amount = Number(input.value);
   if (amount > 0 && amount <= 100) {
-  createBoxes(amount);
-  }
-});
+      createBoxes(amount);
+      input.value = "";
+      } else {
+        alert(`enter a number between 1 and 100`);
+      }
+}
